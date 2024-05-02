8465fea82f4e748f7ec594b3140ff15d4ea431a6
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-02 16:41:44 +0500

    Comments of output

--- Script.js
+++ Script.js
@@ -3,14 +3,16 @@
   //Hoisting is the default behaviour of JavaScript where all the variable and function declarations are moved on top.
 
   //Example 1
-   getName();
-   console.log( num );
-   let num = 3;
+  
+   getName(); // In Simple function output is show 
+   name(); //But In Arrow function output in not show because the name of the function is store in datatype
+   let num = 3; // This is also show error.
    function getName () {
     console.log("Aadarsh Kumar")
    }
    const name = () => {
-     console.log('Aadarsh Kumar')
+     console.log('Aadarsh')
    }
-  console.log(name());  
 
+name()
+  
